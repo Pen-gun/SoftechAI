edab9fe2a7a873f95505500752f516cf1803eb40
Paras Limbu <paras078@academiacollege.edu.np>
2025-12-11 11:56:59 +0545

    feat: initialize backend with Express, dotenv, and essential utilities
    - Created package.json with dependencies and scripts for development.
    - Added app.js to set up Express server with CORS and body parsing.
    - Implemented index.js to configure environment variables and start the server.
    - Introduced ApiError and ApiResponse utility classes for standardized error and response handling.
    - Added asyncHandler utility for handling asynchronous route handlers.
    - Created a .gitkeep file in the public/temp directory to track the folder.

---
+++ Backend/src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL,
+        credentials: true
+    }
+));
+
+app.use(express.json(
+    {
+        limit: '50mb'
+    }
+));
+
+app.use(express.urlencoded(
+    {
+        limit: '50mb',
+        extended: true
+    }
+));
+app.use(express.static('public'));
+export default app;
