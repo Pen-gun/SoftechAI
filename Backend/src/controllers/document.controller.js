5d8332cf5972890c279ded751517e3320c7f2162
Paras Limbu <paras078@academiacollege.edu.np>
2025-12-11 13:00:52 +0545

    refactor: streamline error handling and response structure in document upload and question answering

--- Backend/src/controllers/document.controller.js
+++ Backend/src/controllers/document.controller.js
@@ -6,29 +6,36 @@ import fs from 'fs';
 import { processDocument, askDocument } from '../utils/ApiClient.js';
 
 const uploadDocument = asyncHandler(async (req, res, next) => {
-    try {
-        if(!req.file) {
-            return next(new ApiError(400, 'No file uploaded'));
-        }
-        const filePath = req.file?.path;
-        const data = await processDocument(filePath);
-
-        res.status(200).json(new ApiResponse(true, 'Document processed successfully', data));
-    } catch (error) {
-        next(new ApiError(500, 'Failed to process document'));
+    if (!req.file) {
+        return next(new ApiError('no file uploaded', 400));
     }
-    fs.unlinkSync(req.file.path);
+    const filePath = req.file?.path;
+    const data = await processDocument(filePath);
+    if (!data) {
+        return next(new ApiError('Document processing failed', 500));
+    }
+
+    try {
+        res.status(200).json(new ApiResponse(200, data, 'Document processed successfully'));
+    } catch (error) {
+        console.error('Error sending response:', error);
+        return next(new ApiError('Internal Server Error', 500));
+    }finally{
+        fs.unlinkSync(filePath);
+    }
 });
 
 const askQuestion = asyncHandler(async (req, res, next) => {
-    try {
-        const { document_text, question } = req.body;
-        const data = await askDocument(document_text, question);
-        res.status(200).json(new ApiResponse(true, 'Question answered successfully', data));
-    } catch (error) {
-        next(new ApiError(500, 'Failed to get answer'));
+
+    const { document_text, question } = req.body;
+    if (!document_text || !question){
+        return next(new ApiError('document_text and question are required', 400));
     }
-    fs.unlinkSync(req.file.path);
+    const data = await askDocument(document_text, question);
+    if (!data){
+        return next(new ApiError('Failed to get answer from document', 500));
+    }
+    res.status(200).json(new ApiResponse(200,data, 'Question answered successfully'));
 });
 
 export { uploadDocument, askQuestion };
